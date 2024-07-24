f0b4676097d5ccfac9feb646b078c7834f733a07
안지환 <68071599+jihwooon@users.noreply.github.com>
2024-07-24 11:02:10 +0900

    Initial commit

---
+++ Learn-to-Code-by-Solving-Problems/ch01/basic.py
@@ -0,0 +1,30 @@
+print('hello')
+
+print('a bunch of words')
+
+print('hello ' + 'there')
+
+print('-' * 30)
+
+print('' * 30)
+
+upper = 'hello'.upper()
+print(upper)
+
+strip = '    abc'.strip()
+print(strip)
+
+abc__strip = 'abc'.strip('a')
+print(abc__strip)
+
+abca__strip = 'abca'.strip('a')
+print(abca__strip)
+
+count = 'abc'.count('a')
+print(count)
+
+aaabcaa__count = 'aaabcaa'.count('a')
+print(aaabcaa__count)
+
+ababa__count = 'ababa'.count('aba')
+print(ababa__count)
